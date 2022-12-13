1bb3bb8264550bb012cc3e2e6909b7430ab9ce87
Kcepriu <kcepriu@gmail.com>
2022-12-13 13:51:39 +0200

    ended task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,77 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = findElementWindow();
+
+function findElementWindow() {
+  const elemForm = document.querySelector('.form');
+  return {
+    form: elemForm,
+    inputDelay: elemForm.querySelector('input[name="delay"]'),
+    inputStep: elemForm.querySelector('input[name="step"]'),
+    inputAmount: elemForm.querySelector('input[name="amount"]'),
+    button: elemForm.querySelector('button'),
+  };
+}
+
+// * events
+function addEvents() {
+  refs.form.addEventListener('submit', onSubmit);
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  createPromises();
+}
+
+// * Promise
+function createPromises() {
+  const delay = Number(refs.inputDelay.value);
+  const step = Number(refs.inputStep.value);
+  const amount = Number(refs.inputAmount.value);
+
+  setTimeOutNotify((delay + step * amount) * 0.5);
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, delay + step * i)
+      .then(resolvPromise)
+      .catch(rejectPromise);
   }
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+// * function Promise
+function resolvPromise({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function rejectPromise({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+
+// * Notify
+function setTimeOutNotify(time) {
+  Notify.init({
+    useIcon: false,
+    timeout: time,
+    cssAnimationStyle: 'zoom',
+  });
+}
+
+function initialyzeScript() {
+  addEvents();
+  createPromises();
+}
+
+initialyzeScript();
