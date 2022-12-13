950a3022de6ee02b0033ef2d840556a3651a89dc
Kcepriu <kcepriu@gmail.com>
2022-12-13 10:29:46 +0200

    ended task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+require('flatpickr/dist/themes/material_blue.css');
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -9,6 +10,8 @@ class CountDownTimer {
   constructor(flatpickr) {
     this.refs = this.finndElementsWindow();
     this.flatpickr = flatpickr;
+    this.selectedDates = null;
+    this.inrervalId = null;
   }
 
   finndElementsWindow() {
@@ -22,21 +25,63 @@ class CountDownTimer {
     };
   }
 
-  timer() {
-    // ! Треба змінювати лічильник. Коли дійде до кінця то заупит=нити таймер
-  }
-
   setDefaultActiviteElement() {
-    // ! Установити доступнійть елементів форми
+    // Установити доступнійть елементів форми
+    this.disableElement(this.refs.buttonStart);
+    this.enableElement(this.refs.inputData);
   }
 
   showAlarm(message) {
     Notify.failure(message);
   }
 
-  validData(selectedDates) {
-    // ! Зробити валідацію
-    return false;
+  validateData(selectedDates) {
+    return selectedDates > Date.now();
+  }
+
+  // * Timer
+  startTimer() {
+    // show curent time
+    this.showTime(this.selectedDates - Date.now());
+    // set interval
+    this.inrervalId = setInterval(this.stepTimer.bind(this), 1000);
+  }
+
+  stepTimer() {
+    // Треба змінювати лічильник. Коли дійде до кінця то заупит=нити таймер
+    if (Date.now() > this.selectedDates) {
+      clearInterval(this.inrervalId);
+      // Show 00 00 00
+      this.showTime(0);
+      return;
+    }
+
+    this.showTime(this.selectedDates - Date.now());
+  }
+
+  showTime(time) {
+    const { days, hours, mins, secs } = this.getTimerCOmponents(time);
+    this.refs.days.textContent = days;
+    this.refs.hours.textContent = hours;
+    this.refs.minutes.textContent = mins;
+    this.refs.seconds.textContent = secs;
+  }
+
+  addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  getTimerCOmponents(time) {
+    const days = this.addLeadingZero(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.addLeadingZero(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.addLeadingZero(
+      Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
+    );
+    const secs = this.addLeadingZero(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
   }
 
   //* Events
@@ -49,17 +94,35 @@ class CountDownTimer {
   }
 
   onClickStart(events) {
-    // ! робимо кнопку і поле вводу недоступним
-    // ! Запускаємо таймер
+    // робимо кнопку і поле вводу недоступним
+    this.disableElement(this.refs.buttonStart);
+    this.disableElement(this.refs.inputData);
+
+    // Запускаємо таймер
+    this.startTimer();
   }
 
   onSelectedDataTime(selectedDates) {
-    if (this.validData(selectedDates)) {
-      // ! 2. Якщо дата валідна то Робимо кнопку активною
+    if (this.validateData(selectedDates[0])) {
+      // Якщо дата валідна то Робимо кнопку активною
+      this.enableElement(this.refs.buttonStart);
+      this.selectedDates = selectedDates[0];
     } else {
-      //1. Якщо дата не валідна то покажи повідомлення
+      // Якщо дата не валідна то покажи повідомлення
       this.showAlarm(this.TEXT_MESSAGE_NO_VALID_DATA);
+      // Кнопку зробимо неактивною, бо могли раніше вибрати валідну дату і вона тепер активна
+      this.disableElement(this.refs.buttonStart);
+      this.selectedDates = null;
     }
+  }
+
+  // * Disable Enambe elements
+  disableElement(element) {
+    element.setAttribute('disabled', '');
+  }
+
+  enableElement(element) {
+    element.removeAttribute('disabled');
   }
 
   // * flatpickr
@@ -83,7 +146,7 @@ class CountDownTimer {
     //* Установити доступність кнопки і поля вводу
     this.setDefaultActiviteElement();
 
-    //* Вішаємо  обробник
+    //* Вішаємо  обробники
     this.addEvents();
 
     // * Підключаємо бібліотеку вводу дати
